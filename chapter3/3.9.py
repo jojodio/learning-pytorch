426157a4b806b0f65a3fe30e379cc53674a63aa2
jojodio <2212161496@qq.com>
2019-10-30 23:48:18 +0800

    bad

--- chapter3/3.9.py
+++ chapter3/3.9.py
@@ -5,7 +5,8 @@ sys.path.append("..") # 为了导入上层目录的d2lzh_pytorch
 import d2lzh_pytorch as d2l
 
 print(torch.__version__)
-
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
 batch_size =256
 train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
 
